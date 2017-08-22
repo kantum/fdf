452042e003abe00924399a84317a04c4dab1a2bc
Kantum <username@users.noreply.github.com>
2017-08-22 16:50:25 +0200

    Aurillac

--- srcs/main.c
+++ srcs/main.c
@@ -2,9 +2,13 @@
 
 void	quit(t_env *e)
 {
-	mlx_destroy_window (e->mlx, e->win);
-	exit (0);
+	mlx_destroy_window(e->mlx, e->win);
+	exit(0);
 }
+
+/*
+**		Keyboard inputs hooked here
+*/
 
 int		key_hook(int keycode, t_env *e)
 {
@@ -14,6 +18,10 @@ int		key_hook(int keycode, t_env *e)
 		mlx_clear_window(e->mlx, e->win);
 	return (0);
 }
+
+/*
+**		Mouse inputs hooked here
+*/
 
 int		mouse_hook(int button, int x, int y, t_env *e)
 {
@@ -27,6 +35,10 @@ int		mouse_hook(int button, int x, int y, t_env *e)
 	return (0);
 }
 
+/*
+**		Expose
+*/
+
 int		expose_hook(t_env *e)
 {
 	(void)e;
@@ -34,24 +46,51 @@ int		expose_hook(t_env *e)
 	return (0);
 }
 
+void	show(t_env *e)
+{
+	int			i;
+	int			k;
+
+	i = 0;
+	k = 0;
+	while (e->tab[i])
+	{
+		while (e->tab[i][k])
+		{
+			e->x1 = e->x + e->scale;
+			e->y1 = e->y;
+			mlx_pixel_put(e->mlx, e->win, e->x, e->y, e->color);
+			k++;
+		}
+		e->y += e->scale;
+		i++;
+	}
+}
+
 int		main(int argc, char **argv)
 {
 	t_env		*e;
 
-	if (!(e = (t_env*)malloc(sizeof(t_env))))
+	if (argc < 2 || argc > 2)
+		return (-1);
+	if (!(e = (t_env*)malloc(sizeof (t_env))))
 		return (-1);
 	if (argc == 2)
 		e = parser(argv[1], e);
-
-	e->x = 640;
-	e->y = 400;
-	e->color = 0xCCFFCC;
+	e->win_width = 1280;
+	e->win_height = 800;
+	e->scale = 10;
+	e->color = 0xccffcc;
+	e->x = e->win_width/2 - e->width/2;
+	e->x = (e->win_width/2) + ((e->width/2) * e->scale);
+	e->y = (e->win_height/2) + ((e->height/2) * e->scale);
 	e->mlx = mlx_init();
 	e->win = mlx_new_window(e->mlx, 1280, 800, "win");
-
+	if (e)
+		show(e);
 	mlx_key_hook(e->win, key_hook, e);
 	mlx_mouse_hook(e->win, mouse_hook, e);
-	mlx_expose_hook(e->win, expose_hook, e);
+	//mlx_expose_hook(e->win, expose_hook, e);
 	mlx_loop(e->mlx);
 	free(e);
 }
--- srcs/parser.c
+++ srcs/parser.c
@@ -1,27 +1,107 @@
 #include "fdf.h"
 #include "libft.h"
 
+char	*cat_read(int fd)
+{
+	char	*buf;
+	int		ret;
+	void	*free_ptr;
+	char	rd[BUFF_SIZE + 1];
+
+	buf = ft_strnew(0);
+	while ((ret = read(fd, rd, BUFF_SIZE)))
+	{
+		rd[ret] = '\0';
+		free_ptr = (void*)buf;
+		if (!(buf = ft_strjoin(buf, rd)))
+			return (NULL);
+		free(free_ptr);
+	}
+	return (buf);
+}
+
+int		get_height(char *str)
+{
+	int	i;
+	int	k;
+
+	i = 0;
+	k = 0;
+	while(str[i])
+	{
+		if (str[i] == '\n' )
+			k++;
+		i++;
+	}
+	if ((str[i - 1] != '\n'))
+		k++;
+	return (k);
+}
+
+int		get_length(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\n')
+		i++;
+	return (i);
+}
+
+int		count_field(char const *s, char c)
+{
+	int	i;
+	int	read;
+	int	field;
+
+	i = 0;
+	read = 0;
+	field = 0;
+	while (s[i] && s[i] != '\n')
+	{
+		if (s[i] == c)
+			read = 0;
+		else if (!(s[i] == c) && read == 0)
+		{
+			read = 1;
+			field++;
+		}
+		i++;
+	}
+	return (field);
+}
+
 t_env	*parser(char *arg, t_env *e)
 {
 	int		i;
 	int		k;
-	char	**lines;
+	char	*lines;
 	char	**tmp;
 
-	ft_putstr("hello");
-	lines = NULL;
-	e->fd = open(arg, O_RDONLY);
 	i = 0;
 	k = 0;
-	while (get_next_line(e->fd, lines))
+	e->fd = open(arg, O_RDONLY);
+	lines = cat_read(e->fd);
+	e->width = count_field(lines, ' ');
+	e->height = get_height(lines);
+	if (!(e->tab = (int**)malloc(sizeof(int*) * e->width + 1)))
+		return (NULL);
+	close(e->fd);
+	e->fd = open(arg, O_RDONLY);
+	while (get_next_line(e->fd, &lines))
 	{
-		tmp = ft_strsplit(*lines, ' ');
+		e->tab[k] = (int*)ft_memalloc((sizeof(int) * e->width));
+		tmp = ft_strsplit(lines, ' ');
 		while (tmp[i])
 		{
-			e->tab[k][i] = atoi(tmp[i]);
+			e->tab[k][i] = ft_atoi(tmp[i]);
+			i++;
 		}
+		free(tmp);
 		k++;
 	}
 	e->tab[k] = NULL;
+	close(e->fd);
 	return (e);
 }
+
